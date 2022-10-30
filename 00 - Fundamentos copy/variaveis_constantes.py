9373c4898224c967f6aff62418b946fe83493ea2
Isabella Firmino <isabella.mfbr@gmail.com>
2022-10-30 15:57:48 -0400

    Adicionando fundamentos de pyhton e inicializando estrutura de dados

---
+++ 00 - Fundamentos copy/variaveis_constantes.py
@@ -0,0 +1,12 @@
+nome = "Isabella"
+idade = 22
+
+nome, idade = "Monyk", 25
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+
+print(BRAZILIAN_STATES)
