79aaecc5ee2799f2d76e02cf267e3f1b44fffc5f
Isabella Firmino <isabella.mfbr@gmail.com>
2022-11-02 17:33:07 -0400

    Ajustando setup.py

--- pacotes_process_img/img-processing-pcg/setup.py
+++ pacotes_process_img/img-processing-pcg/setup.py
@@ -14,7 +14,7 @@ setup(
     description="Test version - Image processing. This project belongs to Karina Tiemi Kato, Tech Lead, Machine Learning Engineer, Data Scientist Specialist. This package is a demo for simulation of upload on the Test Pypi website, and it's from class of the Bootcamp Geração Tech Unimed-BH - Ciência de Dados.",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    # url="https://github.com/isobew/trilha-python/tree/main/pacotes_process_img/image-processing-package"
+    # url="https://github.com/isobew/trilha-python/tree/main/pacotes_process_img/img-processing-pcg"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
